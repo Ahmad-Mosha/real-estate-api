72d8a5f4f303ba95f1505fa14e7730ea3052e4cd
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2024-07-02 03:45:55 +0300

    adding guards to routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,31 +1,45 @@
-import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':username')
   findOne(@Param('username') username: string) {
     return this.usersService.findOne(username);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOneById(@Param('id') id: number) {
     return this.usersService.findOneById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: number, @Body() payload: UpdateUserDto) {
     return this.usersService.update(id, payload);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.usersService.remove(id);

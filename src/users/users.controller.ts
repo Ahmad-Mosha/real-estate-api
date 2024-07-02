df1f1c79dc354976512763d5c9903c6a6ae15d8c
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2024-07-02 03:44:11 +0300

    implement some controller methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':username')
+  findOne(@Param('username') username: string) {
+    return this.usersService.findOne(username);
+  }
+
+  @Get(':id')
+  findOneById(@Param('id') id: number) {
+    return this.usersService.findOneById(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() payload: UpdateUserDto) {
+    return this.usersService.update(id, payload);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.usersService.remove(id);
+  }
 }
